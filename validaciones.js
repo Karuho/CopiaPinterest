3ba99d6db774aeca31615ed0bd0d0bb88013e56f
Carlos Torres <64610305+Karuho@users.noreply.github.com>
2021-09-29 13:05:16 -0300

    Add files via upload

---
+++ validaciones.js
@@ -0,0 +1,11 @@
+function login (URL){ 
+   window.open(URL,"Pinterest Login","width=500,height=600,scrollbars=NO") 
+} 
+
+function facebook (URL){ 
+   window.open(URL,"Iniciar sesión en Facebook|Facebook","width=500,height=600,scrollbars=NO") 
+} 
+
+function google (URL){ 
+   window.open(URL,"Inicia sesión: Cuentas de Google","width=500,height=600,scrollbars=NO") 
+} 
